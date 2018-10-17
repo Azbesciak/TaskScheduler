684c96951e5187e2bacd86fac0d9684fc9838d69
Witold Kupś <witold.kups@interlan.pl>
2018-10-17 14:44:25 +0200

    Remove unused regex

--- src/main/scala/cs/put/ptsz/taskscheduler/ProblemParser.scala
+++ src/main/scala/cs/put/ptsz/taskscheduler/ProblemParser.scala
@@ -3,13 +3,9 @@ package cs.put.ptsz.taskscheduler
 import java.io.{File, FileInputStream}
 import java.util.Scanner
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex
-
-import Control.using
+import cs.put.ptsz.taskscheduler.Control.using
 
 object ProblemParser {
-	private val regex = new Regex("\\s+")
-
 	def load(file: File): Array[Problem] = {
 		using(new Scanner(new FileInputStream(file)))(getProblems)
 	}
