3299b9b38cb95e8efc1dc6fd308b50618606564f
Witold Kupś <witold.kups@interlan.pl>
2018-10-18 19:19:42 +0200

    Add basic solver for one machine scheduling

--- src/main/scala/cs/put/ptsz/taskscheduler/Main.scala
+++ src/main/scala/cs/put/ptsz/taskscheduler/Main.scala
@@ -7,6 +7,10 @@ object Main extends App {
 	private val problems = ProblemParser.load(file)
 	private val instanceCreator = new InstanceCreator(problems)
 	private val instance = instanceCreator.get()
-	println(s"instance: $instance")
+	private val costFunction = new OneMachineScheduleEndTimeCostFunction(instance)
+	private val stopCondition = new SolutionsCountStopCondition(1)
+	private val scheduler = new TaskScheduler(instance, stopCondition, costFunction)
+	private val solution = scheduler.schedule()
+	println(solution)
 }
 
--- src/main/scala/cs/put/ptsz/taskscheduler/Problem.scala
+++ src/main/scala/cs/put/ptsz/taskscheduler/Problem.scala
@@ -1,8 +1,19 @@
 package cs.put.ptsz.taskscheduler
 
 case class Task(id: Int, time: Int, earlinessCost: Int, tardinessCost: Int)
+
 case class Problem(tasks: Array[Task]) {
 	override def toString: String = s"Problem(${tasks.mkString("[", ",", "]")})"
 }
-case class Instance(problem: Problem, h: Double)
-case class Solution(tasks: Array[Task])
+
+case class Instance(problem: Problem, h: Double) {
+	val summaryTime: Int = problem.tasks.map(_.time).sum
+}
+
+case class Solution(tasks: Array[TaskSchedule]) {
+	override def toString: String = s"Solution${tasks.mkString("[", ",", "]")}"
+}
+
+case class TaskSchedule(task: Task, start: Int) {
+	val end: Int = start + task.time
+}
