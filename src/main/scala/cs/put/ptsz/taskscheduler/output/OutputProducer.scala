7c332b9ad2b8019ae95511215670e869a41417c2
Witold Kupś <witold.kups@interlan.pl>
2018-10-31 16:26:13 +0100

    Add upper bounds to output solutions

--- src/main/scala/cs/put/ptsz/taskscheduler/output/OutputProducer.scala
+++ src/main/scala/cs/put/ptsz/taskscheduler/output/OutputProducer.scala
@@ -3,6 +3,7 @@ package cs.put.ptsz.taskscheduler.output
 import java.io.{File, FileOutputStream}
 
 import cs.put.ptsz.taskscheduler.solver.{EvaluatedSolution, Instance, Result}
+import cs.put.ptsz.taskscheduler.{InstanceParams, OptimumSource}
 
 import scala.concurrent.duration.Duration
 import scala.util.Properties
@@ -13,9 +14,13 @@ object OutputProducer {
 	val MEASURE_TIME_PROP = "measureTime"
 
 	def consume(results: Array[Result]) = {
-		val serialized = results.map(r => make(r.instance, r.solution, r.duration)).mkString("\n").getBytes
+		val outputValues = results.map(r => make(r.instance, r.solution, r.duration))
+		val padded: Array[String] = padValues(outputValues)
+		val serialized = padded.mkString("\n").getBytes
 		output(results).write(serialized)
 	}
+
+	def enable(property: String) = Properties.setProp(property, "true")
 
 	private def output(results: Array[Result]) = {
 		val instances = results.map(_.instance)
@@ -41,22 +46,46 @@ object OutputProducer {
 	}
 
 	private def make(instance: Instance, solution: EvaluatedSolution, duration: Duration) = {
-		val maybeDetails = onPropertySwitched(DETAILS_PROP, () => s"n:${instance.problem.tasks.length} k:${instance.problem.id} h:${instance.h}")
-		val maybeDuration = onPropertySwitched(MEASURE_TIME_PROP, () => s"TIME: ${getTime(duration)}")
-		val result = Some(s"${solution.cost.value} ${instance.h} ${solution.offset} ${solution.solution.tasks.map(_.task.id).mkString(" ")}")
-		Array(maybeDetails, maybeDuration, result).flatten.mkString("\t| ")
+		val maybeDetails = onPropertySwitched(DETAILS_PROP, () => Array(
+			s"n:${instance.problem.tasks.length}",
+			s"k:${instance.problem.id}",
+			s"h:${instance.h}")
+		)
+		val maybeDuration = onPropertySwitched(MEASURE_TIME_PROP, () => Array(s"TIME:${getTime(duration)}"))
+		val maybeCompare = compareWithUpperBound(instance, solution)
+		val result = getSolutionString(instance, solution)
+		Array(maybeDetails, maybeCompare, maybeDuration, result).flatten.flatten
+	}
+
+	private def getSolutionString(instance: Instance, solution: EvaluatedSolution) = {
+		Some(Array(s"${solution.cost.value} ${instance.h} ${solution.offset} ${solution.solution.tasks.map(_.task.id).mkString(" ")}"))
+	}
+
+	private def compareWithUpperBound(instance: Instance, solution: EvaluatedSolution): Option[Array[String]] = {
+		if (!OptimumSource.canUse())
+			return None
+		val upperBound = OptimumSource.apply(InstanceParams(instance.problem.tasks.length, instance.problem.id, instance.h))
+		val worseBy = (solution.cost.value - upperBound.value) / upperBound.value.toDouble * 100
+		Some(Array(s"Fub:$upperBound",s"Fa:${solution.cost.value}", f"err:$worseBy%.2f%%"))
 	}
 
 	private def getTime(duration: Duration) =
 		if (duration.toMillis < 10)
-			s"${duration.toNanos} ns"
+			s"${duration.toMicros} μs"
 		else if (duration.toMillis < 10000)
 			s"${duration.toMillis} ms"
 		else
 			s"${duration.toSeconds} s"
 
-	private def onPropertySwitched(prop: String, block: () => String) =
+	private def onPropertySwitched(prop: String, block: () => Array[String]) =
 		Properties.propOrNone(prop).filter(_ == "true").map(_ => block())
 
-	def enable(property: String) = Properties.setProp(property, "true")
+	private def padValues(outputValues: Array[Array[String]]) = {
+		val maxLengths = outputValues.map(_.map(_.length)).transpose.map(_.max)
+		val padded = outputValues
+		 .map(_.zip(maxLengths)
+			.map(t => t._1.padTo(t._2 + 1, ' '))
+			.mkString(" | "))
+		padded
+	}
 }
