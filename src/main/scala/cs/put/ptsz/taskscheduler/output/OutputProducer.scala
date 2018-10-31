10401cd01cf48caba8c178346e2c9bd93501a83d
Witold Kupś <witold.kups@interlan.pl>
2018-10-31 14:09:42 +0100

    Add format to output file

--- src/main/scala/cs/put/ptsz/taskscheduler/output/OutputProducer.scala
+++ src/main/scala/cs/put/ptsz/taskscheduler/output/OutputProducer.scala
@@ -9,13 +9,30 @@ import scala.util.Properties
 object OutputProducer {
 	def consume(results: Array[Result]) = {
 		val serialized = results.map(r => make(r.instance, r.solution)).mkString("\n").getBytes
-		output().write(serialized)
+		output(results).write(serialized)
 	}
 
-	private def output() = {
-		Properties.propOrNone("outputFilePath").map(p => {
-			new FileOutputStream(new File(p))
+	private def output(results: Array[Result]) = {
+		val instances = results.map(_.instance)
+		val ids = instances.map(_.problem.id).distinct
+		val hs = instances.map(_.h).distinct.map(_.toString.drop(2).toInt)
+		val ns = instances.map(_.problem.tasks.length).distinct
+		Properties.propOrNone("outputDir").map(p => {
+			val file = getFile(ns, ids, hs, p)
+			new FileOutputStream(file)
 		}).getOrElse(Console.out)
+	}
+
+	private def getFile(ns: Array[Int], ids: Array[Int], hs: Array[Int], dir: String): File = {
+		new File(dir).mkdirs()
+		val format = s"$dir/n${ns.mkString(",")}k${ids.mkString(",")}h${hs.mkString(",")}"
+		var file = new File(s"$format.txt")
+		var index = 0
+		while (file.exists()) {
+			index += 1
+			file = new File(s"$format($index).txt")
+		}
+		file
 	}
 
 	private def make(instance: Instance, solution: EvaluatedSolution) =
