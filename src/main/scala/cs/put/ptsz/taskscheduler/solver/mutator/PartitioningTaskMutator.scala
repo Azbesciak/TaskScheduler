8d264bbfaf44d10837d4b086b9fadee145cddaac
Witold Kupś <witold.kups@interlan.pl>
2018-12-02 20:28:14 +0100

    Add equal time tasks split strategies for partitioningTaskMutator

--- src/main/scala/cs/put/ptsz/taskscheduler/solver/mutator/PartitioningTaskMutator.scala
+++ src/main/scala/cs/put/ptsz/taskscheduler/solver/mutator/PartitioningTaskMutator.scala
@@ -4,12 +4,19 @@ import cs.put.ptsz.taskscheduler.solver.mutator.TasksSorter._
 import cs.put.ptsz.taskscheduler.solver.{Instance, Task}
 
 class PartitioningTaskMutator(val instance: Instance, private val partitioner: Partitioner) extends TasksMutator {
-	private[this] var wasUsed = false
+	private[this] val splitStrategies = Array(
+		AllBeforeStrategy,
+		AllAfterStrategy,
+		new EqualNumberStrategy(0),
+		new EqualNumberStrategy(1)
+	).iterator
 
 	override def mutate(tasks: Array[Task]): Array[Task] = {
+		require(splitStrategies.hasNext, "no other combinations of splitting equal tasks")
 		val PartitionedTasksScheduling(toDoBefore, equal, toDoAfter) = partitioner.tasks
-		wasUsed = true
-		val (sortedToDoBefore, sortedToDoAfter) = sortTasks(toDoBefore, equal ++ toDoAfter)
+		val (beq, aeq) = splitStrategies.next().split(equal)
+		val (sortedToDoBefore, sortedToDoAfter) = sortTasks(toDoBefore ++ beq, toDoAfter ++ aeq)
+		sortedToDoBefore ++ sortedToDoAfter
 		val beforeSum = sortedToDoBefore.map(_.time).sum
 		val afterSum = sortedToDoAfter.map(_.time).sum
 		val timeLeft = instance.dueTime - beforeSum
@@ -35,5 +42,28 @@ class PartitioningTaskMutator(val instance: Instance, private val partitioner: P
 		sortTasks(leftBefore, toDoAfter ++ after)
 	}
 
-	override def canMutate(): Boolean = !wasUsed
+	override def canMutate(): Boolean = splitStrategies.hasNext
 }
+
+trait EqualSplitStrategy {
+	def split(tasks: Array[Task]): (Array[Task], Array[Task])
+}
+
+object AllBeforeStrategy extends EqualSplitStrategy {
+	override def split(tasks: Array[Task]): (Array[Task], Array[Task]) = (tasks, Array.empty)
+}
+
+object AllAfterStrategy extends EqualSplitStrategy {
+	override def split(tasks: Array[Task]): (Array[Task], Array[Task]) = (Array.empty, tasks)
+}
+
+class EqualNumberStrategy(private val initValue: Int) extends EqualSplitStrategy {
+	override def split(tasks: Array[Task]): (Array[Task], Array[Task]) = {
+		var i = initValue
+		tasks.sortBy(_.earlinessCost).partition(_ => {
+			i += 1
+			i % 2 == 0
+		})
+	}
+}
+
